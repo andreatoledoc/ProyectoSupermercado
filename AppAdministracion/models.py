48e507d47f1c4c2bf9f5df9cc1988652a87f2551
Andrea Toledo Cadario <andrea.toledo.cadario@mi.unc.edu.ar>
2023-07-10 21:31:12 -0300

    Clases

---
+++ AppAdministracion/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+class Cliente (models.Model):
+    nombre = models.CharField(max_length=40)
+    telefono = models.IntegerField()
+
+class Proveedor (models.Model):
+    nombre = models.CharField(max_length=40)
+    direccion = models.CharField (max_length=50)
+    email = models.EmailField ()
+    telefono = models.IntegerField()
+
+class Articulo (models.Model):
+    nombre = models.CharField(max_length=40)
+    codigo = models.IntegerField()
+    categoria = models.CharField(max_length=40)
+
+class Empleado (models.model):
+    nombre = models.CharField(max_length=40)
+    direccion = models.CharField (max_length=50)
+    email = models.EmailField ()
+    telefono = models.IntegerField()
+    puesto = models.CharField(max_length=40)
+
+
+
+
